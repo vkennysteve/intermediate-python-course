1877dafa31a3859acf6207091dba497f5a4b4eab
Victor Kennysteve <64693405+vkennysteve@users.noreply.github.com>
2021-08-17 12:59:56 +0800

    Update dice_roller.py

--- dice_roller.py
+++ dice_roller.py
@@ -5,9 +5,13 @@ dice_sum = 0
 for i in range(0, dice_roll):
   roll = random.randint(1, 6)
   dice_sum += roll
-  
-print(f'You rolled a {roll}')
-print(f'You have rolled a sum of {dice_sum}')
+  if roll = 1:
+    print(f'You rolled a {roll}! Critical Failure')
+  elif roll = 6:
+    print(f'You rolled a {roll}! Critical Success!')
+  else:
+  print(f'You rolled a {roll}')
+  print(f'You have rolled a sum of {dice_sum}')
 
 if __name__== "__main__":
   main()
